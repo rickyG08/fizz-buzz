1946aab2e6194d1db385a62c8beefa287f7d250f
Ricky Garcia <rickyg08@msn.com>
2020-09-23 14:52:24 -0600

    Added unit test for FizzBuzz.fromInt.

--- src/edu/cnm/deepdive/controller/Main.java
+++ src/edu/cnm/deepdive/controller/Main.java
@@ -1,7 +1,6 @@
 package edu.cnm.deepdive.controller;
 
-import edu.cnm.deepdive.model.Converter;
-import edu.cnm.deepdive.view.FIzzBuzzString;
+import edu.cnm.deepdive.model.FizzBuzz;
 
 public class Main {
 
@@ -10,7 +9,7 @@ public class Main {
   public static void main(String[] args) {
     int upperLimit = (args.length > 0) ? Integer.parseInt(args [0]) : DEFAULT_UPPER_LIMIT;
     for (int i =1; i <= upperLimit; i++) {
-      System.out.printf("%d : %s%n", i, FIzzBuzzString.format(Converter.convert(i)));
+      System.out.printf("%d : %s%n", i, FizzBuzz.fromInt(i));
     }
   }
 
